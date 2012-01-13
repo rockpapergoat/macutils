14865f54ef982fee0e2912d09c3633977e286d93
nate <nstgermain@ll.mit.edu>
2012-01-13 14:48:12 -0500

    added some methods

--- macutils.rb
+++ macutils.rb
@@ -10,27 +10,54 @@ require 'pathname'
 class Macutils
   
   # user and group tools
+  
+  # returns: string
   def get_current_user
     @user = Etc.getlogin.chomp
   end
-
+  
+  # returns: string
   def get_full_name(user)
     Etc.getpwnam("#{user}")["gecos"].chomp
     # or with dscl
     #%x(dscl . -read /users/#{user} original_realname).split.drop(1).join(" ")
   end
-
+  
+  # returns: string
   def get_homedir(user)
     Etc.getpwnam("#{user}")["dir"].chomp
     # or with dscl
     #homedir = %x(dscl . -read /users/#{user} NFSHomeDirectory).gsub(/NFSHomeDirectory: /,"")
   end
   
+  # returns: array
+  def get_admins
+    `dscl . -read /groups/admin GroupMembership`.split.drop(1)
+  end
+  
+  # returns: hash of hashes in this form {"user" => 1209765}
+  def get_all_over500_users
+    user_hash = {}
+    `dscl . -list /users UniqueID | sort -k2 -n`.split("\n").each do |pair|
+      pair.split(" ").collect! { user_hash[pair.split(" ")[0]] = pair.split(" ")[1].to_i}
+    end
+    user_hash.delete_if {|k,v| v.to_i < 500 }
+    # can also do it like this, but it reports all directory entries
+    # Etc.passwd { |d| user_hash[d.name] = d.uid if d.uid > 500 }
+  end
+  
+  
+  
   # networking
+  
+  # returns: array
   def get_interfaces
       @list = %x(/usr/sbin/networksetup -listallnetworkservices).split("\n").drop(1)
   end
   
+  # returns: string
+  # for now, just outputs what it would do.
+  # will return maybe nothing unless error.
   def set_proxyurl(interfaces)
       if interfaces.is_a?(Array)
         interfaces.each do |nic|
@@ -81,8 +108,19 @@ class Macutils
   end
   
   # misc. tools
-  def stash_user_file(*args)
-    # not really needed, since casper handles FEU and FUT
+  
+  # returns: string
+  # notifies of copying file or files to spot under user's homedir.
+  # assumes passing full path to the file to be copied.
+  # needs work before using.
+  # fra-gee-lay
+  def stash_user_file(file,path)
+      users = get_all_over500_users
+      users.each_key do |u|
+          puts "copying files to #{u}\'s home at #{path}."
+          system "ditto -V #{file} /Users/#{u}/#{path}/#{File.basename(file)}"
+          FileUtils.chown_R("#{u}", nil, "/Users/#{u}/#{path}/#{File.basename(file)}")
+      end
   end
 
 end
